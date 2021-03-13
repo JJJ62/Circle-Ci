3a9f2eef5fef015b6049880288d7c7e581d57a90
JJJ62 <alamgirj@flip3.engr.oregonstate.edu>
2021-03-12 22:50:28 -0800

    working on it

--- test_calculator.py
+++ test_calculator.py
@@ -2,8 +2,8 @@ import calculator
 
 
 class TestCalculator:
-	def test_add(self):
+    def test_add(self):
         assert 5 == calculator.add(1, 4)
-	
-	def test_subtract(self):
+
+    def test_subtract(self):
         assert 2 == calculator.sub(5, 3)
